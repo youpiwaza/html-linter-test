780f70ec0186565ef6b052db213869fe971bcd60
Max <masamune.code@gmail.com>
2019-08-22 16:28:16 +0200

    FIX: lint indentation & proper error display

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,18 @@ const { dest, parallel, series, src, task, watch } 	= require('gulp'),
 
 task('default', function() {
     return src('src/index.html')
-    .pipe(htmllint(
-        // lint success
-        fancyLog(colors.green('HTML is clean :)')),
-        // lint fail
-        htmllintReporter
-    ));
+    .pipe(htmllint({}, htmllintReporter));
 });
 
+// Handle htmllint errors display
 function htmllintReporter(filepath, issues) {
     if (issues.length > 0) {
         issues.forEach(function (issue) {
             fancyLog(colors.cyan('[gulp-htmllint] ') + colors.white(filepath + ' [' + issue.line + ',' + issue.column + ']: ') + colors.red('(' + issue.code + ') ' + issue.msg));
         });
         process.exitCode = 1;
+    }
+    else {
+      fancyLog(colors.green('HTML is clean :)'))
     }
 }
